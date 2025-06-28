08ad77597712a94e13660929c777ad948105ed91
root <1102197+priyadi@users.noreply.github.com>
2025-06-28 18:40:35 +0000

    fix: move services to the ux package (#381)

--- config/services.php
+++ config/services.php
@@ -13,14 +13,47 @@ declare(strict_types=1);
 
 namespace Rekalogika\Analytics\Bundle;
 
+use Rekalogika\Analytics\Contracts\DistinctValuesResolver;
+use Rekalogika\Analytics\Frontend\Formatter\Stringifier;
+use Rekalogika\Analytics\Metadata\Summary\SummaryMetadataFactory;
+use Rekalogika\Analytics\UX\PanelBundle\Filter\DateRange\DateRangeFilterFactory;
+use Rekalogika\Analytics\UX\PanelBundle\Filter\Equal\EqualFilterFactory;
+use Rekalogika\Analytics\UX\PanelBundle\Filter\Null\NullFilterFactory;
+use Rekalogika\Analytics\UX\PanelBundle\Filter\NumberRanges\NumberRangesFilterFactory;
+use Rekalogika\Analytics\UX\PanelBundle\FilterFactory;
+use Rekalogika\Analytics\UX\PanelBundle\Internal\DefaultFilterFactory;
+use Rekalogika\Analytics\UX\PanelBundle\PivotAwareQueryFactory;
 use Rekalogika\Analytics\UX\PanelBundle\Twig\AnalyticsExtension;
 use Rekalogika\Analytics\UX\PanelBundle\Twig\AnalyticsRuntime;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
 use function Symfony\Component\DependencyInjection\Loader\Configurator\service;
+use function Symfony\Component\DependencyInjection\Loader\Configurator\tagged_locator;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
     $services = $containerConfigurator->services();
+
+    //
+    // pivot table
+    //
+
+    $services->alias(
+        PivotAwareQueryFactory::class,
+        'rekalogika.analytics.pivot_aware_query_factory',
+    );
+
+    $services
+        ->set('rekalogika.analytics.pivot_aware_query_factory')
+        ->class(PivotAwareQueryFactory::class)
+        ->args([
+            '$filterFactory' => service(FilterFactory::class),
+            '$summaryMetadataFactory' => service(SummaryMetadataFactory::class),
+        ])
+    ;
+
+    //
+    // twig
+    //
 
     $services
         ->set('rekalogika.analytics.twig.runtime.analytics')
@@ -34,4 +67,52 @@ return static function (ContainerConfigurator $containerConfigurator): void {
         ->set('rekalogika.analytics.twig.extension.analytics')
         ->class(AnalyticsExtension::class)
         ->tag('twig.extension');
+
+    //
+    // filter
+    //
+
+    $services
+        ->set(FilterFactory::class)
+        ->class(DefaultFilterFactory::class)
+        ->args([
+            '$specificFilterFactories' => tagged_locator('rekalogika.analytics.specific_filter_factory', defaultIndexMethod: 'getFilterClass'),
+            '$summaryMetadataFactory' => service(SummaryMetadataFactory::class),
+            '$managerRegistry' => service('doctrine'),
+        ])
+    ;
+
+    $services
+        ->set(DateRangeFilterFactory::class)
+        ->args([
+            '$summaryMetadataFactory' => service(SummaryMetadataFactory::class),
+        ])
+        ->tag('rekalogika.analytics.specific_filter_factory')
+    ;
+
+    $services
+        ->set(EqualFilterFactory::class)
+        ->args([
+            '$summaryMetadataFactory' => service(SummaryMetadataFactory::class),
+            '$distinctValuesResolver' => service(DistinctValuesResolver::class),
+            '$stringifier' => service(Stringifier::class),
+        ])
+        ->tag('rekalogika.analytics.specific_filter_factory')
+    ;
+
+    $services
+        ->set(NumberRangesFilterFactory::class)
+        ->args([
+            '$summaryMetadataFactory' => service(SummaryMetadataFactory::class),
+        ])
+        ->tag('rekalogika.analytics.specific_filter_factory')
+    ;
+
+    $services
+        ->set(NullFilterFactory::class)
+        ->args([
+            '$summaryMetadataFactory' => service(SummaryMetadataFactory::class),
+        ])
+        ->tag('rekalogika.analytics.specific_filter_factory')
+    ;
 };
